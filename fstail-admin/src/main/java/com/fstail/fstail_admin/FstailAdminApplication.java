7108dd170f73f99db3618278683138e361d68a3e
Nahuel Pierni and Anaclara Ferrando <fstailsolution@gmail.com>
2025-10-27 20:14:36 -0300

    create Spring proyect

---
+++ fstail-admin/src/main/java/com/fstail/fstail_admin/FstailAdminApplication.java
@@ -0,0 +1,13 @@
+package com.fstail.fstail_admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FstailAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FstailAdminApplication.class, args);
+	}
+
+}
---
+++ fstail-admin/src/test/java/com/fstail/fstail_admin/FstailAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fstail.fstail_admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FstailAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
